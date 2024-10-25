f11000c7747183af195f49a9c06620846a2420e9
Vedant <borodevedant4@gmail.com>
2024-10-25 23:58:04 +0530

    Basic structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="">
+            <div className="">logo</div>
+            <div className="">social handles</div>
+        </nav>
+    </>
+)
+}
+
+export default Navbar
